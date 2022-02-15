8611a5796031d27d2af8e5738aca9acbcdf8dad2
aarav-rana2010 <78865218+aarav-rana2010@users.noreply.github.com>
2022-02-15 21:24:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 Webcam.set({
-width: 350,
-height: 300,
-image_format:'png',
-png_quality: 90
-});
-camera=document.getElementById("camera");
-Webcam.attach('#camera');
-function take_snapshot(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img src="'+data_uri+'" id="captured_image">'
+    width: 350,
+    height: 300,
+    image_format:'png',
+    png_quality: 90
     });
-}
-console.log('ml5 version',ml5.version);
-var classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/gQfSf6dnh/model.json',ModelLoaded);
-function ModelLoaded(){
-    console.log("Model Loaded Succesfully");
-}
+    camera=document.getElementById("camera");
+    Webcam.attach('#camera');
+    function take_snapshot(){
+        Webcam.snap(function(data_uri){
+            document.getElementById("result").innerHTML='<img src="'+data_uri+'" id="captured_image">'
+        });
+    }
+    console.log('ml5 version',ml5.version);
+    var classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/gQfSf6dnh/model.json',ModelLoaded);
